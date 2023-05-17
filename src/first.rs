47f2494fdc95888c2a272f8d694dcfcb85258bca
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-17 15:26:33 -0400

    Ch3. Iter

--- src/first.rs
+++ src/first.rs
@@ -62,17 +62,17 @@ pub struct Iter<'a, T> {
 }
 
 impl<T> List<T> {
-    pub fn iter(&self) -> Iter<T> {
-        Iter { next: self.head.map(|node| &node) }
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter { next: self.head.as_deref() }
     }
 }
 
-impl<T> Iterator for Iter<T> {
-    type Item = &T;
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next.map(|node| {
-            self.next = node.next.map(|node| &node);
+            self.next = node.next.as_deref();
             &node.elem
         })
     }
@@ -151,6 +151,16 @@ mod test {
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+    }
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 
 
