02eccacbd7e4901fcfc666be4f4178c2c7e53a6f
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-17 15:09:31 -0400

    Ch3. adding lifetimes

--- src/first.rs
+++ src/first.rs
@@ -9,7 +9,7 @@ struct Node<T> {
     next: Link<T>,
 } //making all of our types generic (List, Link and Node)
 
-
+pub struct IntoIter<T>(List<T>);
 
 impl<T> List<T> {
     pub fn new() -> Self {
@@ -39,8 +39,45 @@ impl<T> List<T> {
             &mut node.elem
         })
     }//mutable version of peek
-    
 }
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+
+impl <T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.map(|node| &node) }
+    }
+}
+
+impl<T> Iterator for Iter<T> {
+    type Item = &T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.map(|node| &node);
+            &node.elem
+        })
+    }
+}
+
 
 
 
@@ -103,6 +140,19 @@ mod test {
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
     }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
 
 }
 
